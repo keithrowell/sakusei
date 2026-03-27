89034873259acec402f85d7bacf2d14a381cf61a
Keith Rowell <keith@keithrowell.com>
2026-03-28 04:33:55 +1000

    feat: add components/, package.json, .gitignore to default style pack template

---
+++ lib/templates/default_style_pack/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<docs>
+A simple greeting component. Replace or delete this example.
+
+Props:
+- name (optional): Name to greet. Defaults to "World".
+</docs>
+
+<template>
+  <div class="hello-world">
+    <p>Hello, {{ name }}!</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'World'
+  }
+})
+</script>
+
+<style scoped>
+.hello-world {
+  padding: 0.5em;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  display: inline-block;
+}
+</style>
